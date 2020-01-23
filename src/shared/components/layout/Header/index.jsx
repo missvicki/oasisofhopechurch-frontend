43aa208e2bad1d0812fbb9d0686a1e9a351d82d1
Victor Nomwesigwa <bethel@Bethel.local>
2020-01-23 16:10:32 +0300

    header and welcome section

--- src/shared/components/layout/Header/index.jsx
+++ src/shared/components/layout/Header/index.jsx
@@ -4,38 +4,51 @@ import './Header.scss';
 function Header() {
   return (
     <div className="Header">
-      <div className="row">
-        <div className="head col-md-6">Oasis of Hope Church</div>
-        <div className="menu col-md-3">
-          <ul className="nav justify-content-end u-margin-right">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        {/* <a className="navbar-brand" href="/">
+          <img className="nav-logo" src={OasisLogo} width="200px" />
+        </a> */}
+        <h1>OoHC</h1>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item u-margin-left">
-              <a className="nav-link active" href="#">
-                HOME
+              <a className="nav-link active" href="/">
+                Home
               </a>
             </li>
             <li className="nav-item u-margin-left">
               <a className="nav-link" href="#">
-                MINISTRIES
+                Ministries
               </a>
             </li>
             <li className="nav-item u-margin-left">
               <a className="nav-link" href="#">
-                ABOUT
+                Our Story
               </a>
             </li>
             <li className="nav-item u-margin-left">
               <a className="nav-link" href="#">
-                VISIT US
+                Vist Us
               </a>
             </li>
           </ul>
         </div>
-        <div className="donate col-md-3">
-          <button type="button" className="btn">
-            DONATE
-          </button>
-        </div>
-      </div>
+        <button type="button" className="btn btn-lg donate">
+          Donate
+        </button>
+      </nav>
     </div>
   );
 }
